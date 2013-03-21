04707992aef8fa38f8bba55031b131ed4294bfba
paulo.ditarso <paulo.ditarso@gmail.com>
2013-03-21 18:54:05 +0000

    backup-2013-03-21

--- simulator/src/getbalance.c
+++ simulator/src/getbalance.c
@@ -7,7 +7,7 @@
 
 #include "simulation.h"
 
-unsigned int GetBalance(balanceAccountInfo *ptrBalanceAccountInfo, unsigned long int time) {
+unsigned int GetBalance(balanceAccountInfo **ptrPtrBalanceAccountInfo, unsigned long int time) {
 
 	balanceAccountInfo *ptrAuxBalanceAccount;
 //	ptrAuxBalanceAccount = ptrBalanceAccountInfo;
@@ -20,6 +20,20 @@ unsigned int GetBalance(balanceAccountInfo *ptrBalanceAccountInfo, unsigned long
 		}
 		ptrLastBalance = ptrAuxBalanceAccount;
 		ptrAuxBalanceAccount = ptrAuxBalanceAccount->nextBalanceAccountInfo;
+	}
+
+	if ( (ptrLastBalance->balanceAccountID != 0) && (ptrLastBalance != (*ptrPtrBalanceAccountInfo)) ) {
+
+		while ( (*ptrPtrBalanceAccountInfo)->balanceAccountID < ptrLastBalance->balanceAccountID ) {
+
+			balanceAccountInfo *ptrRemove;
+			ptrRemove = (*ptrPtrBalanceAccountInfo);
+			(*ptrPtrBalanceAccountInfo) = (*ptrPtrBalanceAccountInfo)->nextBalanceAccountInfo;
+			free(ptrRemove);
+			ptrRemove = NULL;
+
+		}
+
 	}
 
 	// returning the accumulated balance at that point
