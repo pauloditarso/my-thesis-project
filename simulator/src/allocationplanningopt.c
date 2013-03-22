004ebd46563ae970de672db2082a2387646b5d6b
paulo.ditarso@gmail.com <paulo.ditarso@gmail.com@22afc891-5bdd-b279-75f0-817c1cddb4fa>
2013-03-22 02:26:19 +0000

    backup-2013-03-21-D

--- simulator/src/allocationplanningopt.c
+++ simulator/src/allocationplanningopt.c
@@ -120,7 +120,7 @@ void AllocationPlanningOpt(event *ptrCurrentEvent, event *ptrEventList, machine 
 			while(ptrAux1->nextMachineOptSet != NULL) ptrAux1 = ptrAux1->nextMachineOptSet;
 
 			unsigned long int i;
-			for (i = 1; (i <= (jobSize-numberOfLocalMachines) && i <= numberOfGridMachines); i++) {
+			for (i = 1; (i <= jobSize && i <= numberOfGridMachines); i++) {
 
 //				 debug mode
 //				if (ptrCurrentEvent->eventNumber == 9749) {
