a0fb713359c36ea5209d0079ce7c75369c81e29e
PedroOtaviano <pedro.redes23@gmail.com>
2015-12-17 10:46:58 -0300

    Teste
    Teste de sincronização

--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -11,6 +11,7 @@
 #include "simulation.h"
 
 //#define CPU_TIME (getrusage(RUSAGE_SELF,&ruse), ruse.ru_utime.tv_sec + ruse.ru_stime.tv_sec + 1e-6 * (ruse.ru_utime.tv_usec + ruse.ru_stime.tv_usec))
+//teste de github
 
 int main(int argc, char *argv[]) {
 
